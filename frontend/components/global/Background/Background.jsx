e4dcb2932476a6bb048b4264930df6f47bc471e8
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-08-28 18:10:02 +0400

    add background image

---
+++ frontend/components/global/Background/Background.jsx
@@ -0,0 +1,10 @@
+import styles from "./Background.module.css"
+import background from "@/public/background.jpg"
+
+export default function Background() {
+  return (
+    <div className={styles.container}>
+        
+    </div>
+  )
+}
