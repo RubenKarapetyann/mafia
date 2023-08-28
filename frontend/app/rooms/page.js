b9cd0858c386edc2ac3f4587af955f17b12db9cd
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-08-29 01:45:02 +0400

    add socket settings at frontend

--- frontend/app/rooms/page.js
+++ frontend/app/rooms/page.js
@@ -1,12 +1,31 @@
+"use client"
 import { getRooms } from "@/api/rooms"
+import { SERVER_URL } from "@/constants/api-constants"
+import { useEffect, useRef } from "react"
+import io from "socket.io-client"
 
-export default async function Rooms(){
-    const { rooms } = await getRooms()
+
+export default function Rooms(){
+    const socketRef = useRef(null)
+    // const { rooms } = await getRooms()
+
+    useEffect(()=>{
+        socketRef.current = io(SERVER_URL)
+
+        // {
+        //     query: { roomId : id }
+        // }
+
+        return () => {
+            socketRef.current.disconnect()
+        }
+    },[])
+
     return (
         <div>
-            {rooms.map(room=>{
+            {/* {rooms.map(room=>{
                 return <p>{room.name}</p>
-            })}
+            })} */}
         </div>
     )
 }
