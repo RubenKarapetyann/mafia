fd8b80e8cf737c2aaaef205854d05ee364e7abd8
rubenKarapetyann <ruben.karapetyannn@gmail.com>
2023-08-29 22:20:40 +0400

    add single room styles

--- frontend/components/rooms/RoomsList/RoomsList.jsx
+++ frontend/components/rooms/RoomsList/RoomsList.jsx
@@ -6,7 +6,11 @@ export default function RoomsList({ rooms }){
         <div>
             {rooms.map(room=>{
                 return <SingleRoom
+                    key={room.id}
                     name={room.name}
+                    description={room.description}
+                    players={room.players.length}
+                    status={room.status}
                 />
             })}
         </div>
--- frontend/components/rooms/SingleRoom/SingleRoom.jsx
+++ frontend/components/rooms/SingleRoom/SingleRoom.jsx
@@ -1,9 +1,28 @@
 import styles from "./SingleRoom.module.css"
+import { FaLock, FaLockOpen } from "react-icons/fa"
 
-export default function SingleRoom({ name }){
+export default function SingleRoom({ name, description, players, status }){
     return (
-        <div>
-            {name}
+        <div className={styles.container}>
+            <div>
+                <p className={styles.name}>{name}</p>
+                <br/>
+                <p className={styles.description}>Описание : {description}</p>
+                <p className={styles.players}>Игроков в комнате : {players} из {10}</p>
+            </div>
+            <div>
+                <p>{status === "open" ? 
+                <p>
+                    <span className={styles.openText}>открыто</span>
+                    <FaLockOpen/>
+                </p>
+                 : 
+                <p>
+                    <span className={styles.withPasswordText}>с паролем</span>
+                    <FaLock/>
+                </p>
+                }</p>
+            </div>
         </div>
     )
 }
